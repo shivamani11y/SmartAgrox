164110e29c04a82667e4e855674092912efd19c9
Elakurthi Shivamani <yadavshivamani444@gmail.com>
2026-07-08 11:52:19 +0530

    Use new Firebase project configuration

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,13 +8,12 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB4qlOrr88XUE9vSQ1jYY1ZzAEBjjkjyaU",
-  authDomain: "smartagrox-c8990.firebaseapp.com",
-  projectId: "smartagrox-c8990",
-  storageBucket: "smartagrox-c8990.appspot.com",
-  messagingSenderId: "1040204518562",
-  appId: "1:1040204518562:web:56e9bd91e9bf4297a0f3fb",
-  measurementId: "G-SJYSJJKGKE"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
